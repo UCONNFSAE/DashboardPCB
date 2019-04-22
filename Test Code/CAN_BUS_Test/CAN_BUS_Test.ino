0061723521b5ff714085cc8892a0003926e6fb1c
UCONNFSAE <32419809+UCONNFSAE@users.noreply.github.com>
2019-04-22 15:15:14 -0400

    Further testing of CAN BUS and I2C LED Driver

--- Test Code/CAN_BUS_Test/CAN_BUS_Test.ino
+++ Test Code/CAN_BUS_Test/CAN_BUS_Test.ino
@@ -17,48 +17,52 @@
  * CAN_SS   / Arduino Pin 8  / PA06
  */
 
-#define CAN_MISO  18
-#define CAN_SCK   9
-#define CAN_MOSI  17
-#define CAN_SS    8
+#define CAN_MISO  12
+#define CAN_SCK   13
+#define CAN_MOSI  11
+//#define CAN_SS    9
+
+const int SPI_CS_PIN = 9;
 
 // Override default SPI settings
-//SPISettings Standard(2000000, MSBFIRST, SPI_MODE0);
+SPISettings Standard(1000000, MSBFIRST, SPI_MODE0);
 
 // Declare a new SPIClass specifically for the CAN Bus Controller
-//SPIClass CAN_SPI(&sercom0, CAN_MISO, CAN_SCK, CAN_MOSI, SPI_PAD_0_SCK_3, SERCOM_RX_PAD_1);
+SPIClass CAN_SPI(&sercom1, CAN_MISO, CAN_SCK, CAN_MOSI, SPI_PAD_0_SCK_1, SERCOM_RX_PAD_3);
 
 // Initialize the CAN Constructor with the predefined SS Pin
-MCP_CAN CAN(CAN_SS);
+MCP_CAN CAN(SPI_CS_PIN);
 
 void setup() {
-  //pinMode(CAN_SS, OUTPUT);
-  //digitalWrite(CAN_SS, HIGH);
-
   //SPI.begin();
 
-  SerialUSB.begin(9600);
+  SerialUSB.begin(115200);
   while(!SerialUSB);
 
-  SerialUSB.println("test");
+  //Serial.begin(115200);
 
+  CAN.setSPI(&CAN_SPI);
+  //Changed the SPIClass used by the CAN Bus Shield from the default
   //CAN_SPI.begin();
 
   // Pin Peripheral must be manually changed after CAN_SPI.begin()
-  //pinPeripheral(CAN_MISO, PIO_SERCOM_ALT);
-  //pinPeripheral(CAN_SCK, PIO_SERCOM_ALT);
-  //pinPeripheral(CAN_MOSI, PIO_SERCOM_ALT);
+  //pinPeripheral(CAN_MISO, PIO_SERCOM);
+  //pinPeripheral(CAN_SCK, PIO_SERCOM);
+  //pinPeripheral(CAN_MOSI, PIO_SERCOM);
 
-  //Changed the SPIClass used by the CAN Bus Shield from the default
-  //CAN.setSPI(&CAN_SPI);
+  
 
-  while (CAN_OK != CAN.begin(CAN_10KBPS))
-  {
-    SerialUSB.println("CAN Bus Failed to Initialize");
+  //SerialUSB.println(CAN.begin(CAN_100KBPS));
+  //SerialUSB.println("Hello?");
+  
+  while (CAN_OK != CAN.begin(CAN_1000KBPS)) {             // init can bus : baudrate = 500k
+    //Serial.println("CAN BUS Shield init fail");
+    SerialUSB.println("CAN BUS Shield init fail");
+    SerialUSB.println("Init CAN BUS Shield again");
     delay(100);
   }
-  SerialUSB.println("Initialized");
-
+  //Serial.println("CAN BUS Shield init ok!");
+  SerialUSB.println("CAN BUS Shield init ok!");
 }
 
 unsigned char test[8] = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -77,7 +81,7 @@ void loop() {
 //
 //  SPI.endTransaction();
 
-  SerialUSB.println("test2");
+//  SerialUSB.println("test2");
   
   test[7] = test[7] + 1;
 
--- Test Code/I2C_Warning_Panel_Test/I2C_Warning_Panel_Test.ino
+++ Test Code/I2C_Warning_Panel_Test/I2C_Warning_Panel_Test.ino
@@ -1,63 +1,129 @@
 
 #include <Wire.h>
+#include <SPI.h>
+#include "wiring_private.h"
 
 #define MC 3
+#define MISO ATN
+#define SCK 5
+#define MOSI 2
+/*
 #define device_address 0x78
+#define address2 0x02
+#define modeset 0x06
+#define address3 0x03
 #define brightness 0x26   // 38/64
 #define PWM 0xBE  // 256/512
+#define sleep 0xFF
+*/
+SPISettings LED(1000000, MSBFIRST, SPI_MODE0);
+SPIClass LED_SPI(&sercom4, MISO, SCK, MOSI, SPI_PAD_2_SCK_3, SERCOM_RX_PAD_0);
 
-int val = 0;
+int8_t device_address = 0x78;
+int8_t address2 = 0x02;
+int8_t mode = 0x06;
+int8_t address3 = 0x03;
+int8_t brightness = 0x26;
+int8_t PWM = 0xBE;
+int8_t sleep = 0xFF;
+
+//int data[] = {0,1,1,1,1,0,0,0, 0,0,0,0,0,0,1,0, 0,0,0,0,0,1,1,0, 0,0,0,0,0,0,1,1, 0,0,1,0,0,1,1,0, 0,0,1,0,0,1,1,0, 0,0,1,0,0,1,1,0, 1,1,1,1,1,1,1,1};
+//            address          register 0x02    ENMD-1 MD-10     register 0x03    brightness       brightness       brightness       sleep
+
 
 void setup() {
+  //SerialUSB.begin();
+  //while(!SerialUSB());
   pinMode(MC, OUTPUT);
   digitalWrite(MC, LOW);
+
+  LED_SPI.begin();
+  pinPeripheral(SCK, PIO_SERCOM_ALT);
+  pinPeripheral(MOSI, PIO_SERCOM_ALT);
+  delay(50);
+  LED_SPI.beginTransaction(LED);
+  LED_SPI.transfer(0xFF);
+  LED_SPI.transfer(0xFF);
+  LED_SPI.transfer(device_address);
+  LED_SPI.transfer(address2);
+  LED_SPI.transfer(mode);
+  LED_SPI.transfer(address3);
+  LED_SPI.transfer(brightness);
+  LED_SPI.transfer(brightness);
+  LED_SPI.transfer(brightness);
+
+  for(int i = 0; i < 24; i++) {
+    LED_SPI.transfer(PWM);
+  }
+
+  LED_SPI.endTransaction();
+  
+/*
+  Wire.begin();
+  Wire.beginTransmission(0xFF);
+  Wire.write(0xFF);
+  Wire.endTransmission(false);
+
+  Wire.beginTransmission(device_address);
+  Wire.write(0x02); // register 0x02
+  Wire.write(0x06); // ENMD-1 MD-1
+  Wire.write(0x03); // register 0x03
+  Wire.write(brightness);
+  Wire.write(brightness);
+  Wire.write(brightness);
+  Wire.write(0x0F); // sleep
+  Wire.endTransmission();
+
   pinMode(SDA, OUTPUT);
   pinMode(SCL, OUTPUT);
 
   digitalWrite(SDA, HIGH);
   delayMicroseconds(1);
-  for(int i = 0; i < 16; i++) {
+
+  for(int i = 0; i < 16; i++) {   // device start condition
     digitalWrite(SCL, HIGH);
     delayMicroseconds(1);
     digitalWrite(SCL, LOW);
     delayMicroseconds(1);
   }
-  Wire.begin();
+  digitalWrite(SCL, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(SDA, LOW); // I2C start condition
+  delayMicroseconds(1);
+  digitalWrite(SCL, LOW);
+  delayMicroseconds(1);
+
+  for(int k = 0; k < 64; k++) {
+    if(data[k] == 1)
+      digitalWrite(SDA, HIGH);
+    else
+      digitalWrite(SDA, LOW);
+
+    delayMicroseconds(1);
+    digitalWrite(SCL, HIGH);
+    delayMicroseconds(1);
+    digitalWrite(SCL, LOW);
+  }
+  digitalWrite(SCL, LOW);
+  digitalWrite(SDA, LOW);
+  delayMicroseconds(1);
+  digitalWrite(SCL, HIGH);
+  delayMicroseconds(1);
+  digitalWrite(SDA, HIGH);
+*/
 /*
-  Wire.beginTransmission(0xFF);
-  Wire.endTransmission();
-  Wire.beginTransmission(0xFF);
-  Wire.endTransmission();
   Wire.beginTransmission(device_address);
   Wire.write(0x02); // register address
-  Wire.write(0x02); // increment mode 2
-  Wire.write(brightness);
-  Wire.write(brightness);
-  Wire.write(brightness);
-
-  for(int i = 0; i <= 23; i++)
-  {
-    Wire.write(PWM);
-  }
-
-  Wire.write(0x00); // loopback to 0x06 register
-  Wire.endTransmission
-*/
-}
-
-void loop() {
-  Wire.beginTransmission(device_address>>1);
-  Wire.write(0x02); // register address
   Wire.write(0x01); // increment mode 1
-  Wire.write(0x03);
-  Wire.write(brightness);
-  Wire.write(brightness);
-  Wire.write(brightness);
+  Wire.write(0x03); // register address
   for(int i = 0; i <= 23; i++)
   {
     Wire.write(0x00);
   }
   Wire.endTransmission();
+*/
+}
 
-  delay(1000);
+void loop() {
+
 }
